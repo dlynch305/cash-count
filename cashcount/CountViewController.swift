f2ee7dc69bb8a06e0450b5ae78c952aee53d82ed
Dylan Lynch <dlynch305@gmail.com>
2015-12-03 18:30:59 -0800

    Simplify Code

--- cashcount/CountViewController.swift
+++ cashcount/CountViewController.swift
@@ -14,9 +14,9 @@ class CountViewController: UIViewController {
     var HOURLY_WAGE:Float!
     var FEDERAL_TAX:Float!
     
-    var totalWageEarned:Float = 0.0
-    var netWageEarned:Float = 0.0
-    var wageTaxedFederal:Float = 0.0
+    var totalWageEarned:Float!
+    var netWageEarned:Float!
+    var wageTaxedFederal:Float!
     
     // MARK: Properties
     @IBOutlet weak var totalEarnedLabel: UILabel!
@@ -29,20 +29,14 @@ class CountViewController: UIViewController {
         let RED = UIColor(red: 0.85, green: 0.0, blue: 0.0, alpha: 1.0)
         let GREEN = UIColor(red: 0.0, green: 0.85, blue: 0.0, alpha: 1.0)
         
-        totalEarnedLabel.textColor = GREEN
-        totalEarnedLabel.text = String(format: "$%.2f", totalWageEarned)
-        
         federalTaxLabel.textColor = RED
-        federalTaxLabel.text = String(format: "$%.2f", wageTaxedFederal)
+        netEarnedLabel.textColor = GREEN
         
-        netEarnedLabel.textColor = RED
-        netEarnedLabel.text = String(format: "$%.2f", netWageEarned)
-        
-        NSTimer.scheduledTimerWithTimeInterval(1.25, target: self, selector: Selector("Main"), userInfo: nil, repeats: true)
+        NSTimer.scheduledTimerWithTimeInterval(1.25, target: self, selector: Selector("main"), userInfo: nil, repeats: true)
     }
 
     // MARK: Functions
-    func Main() {
+    func main() {
         calcWagePerSecond()
         calcFederalTax()
         
